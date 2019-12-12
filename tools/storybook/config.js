f6f7af98d3223cc965b0bbd476c4b76ac97b287d
Evan Wallace <evan.wallace@leapdev.io>
2019-12-12 15:31:05 +1100

    storybook viewport addon

--- tools/storybook/config.js
+++ tools/storybook/config.js
@@ -1,3 +1,42 @@
-import { configure } from '@storybook/angular';
+import { configure, addParameters } from '@storybook/angular';
+
+const newViewports  = {
+    sm: {
+        name: 'sm',
+        styles: {
+            width: '640px',
+            height: '100%',
+        }
+    },
+    md: {
+        name: 'md',
+        styles: {
+            width: '768px',
+            height: '100%',
+        }
+    },
+    lg: {
+        name: 'lg',
+        styles: {
+            width: '1024px',
+            height: '100%',
+        }
+    },
+    xl: {
+        name: 'xl',
+        styles: {
+            width: '1280px',
+            height: '100%',
+        }
+    },
+};
+
+addParameters({
+    viewport: {
+      viewports: {
+        ...newViewports,
+      },
+    },
+  });
 
 configure(require.context('../../libs', true, /\.stories\.[tj]s$/), module);
